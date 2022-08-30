368a6ad083134b48194d202b96dca2567b8d9454
Daniyal0214 <112514511+Daniyal0214@users.noreply.github.com>
2022-08-31 01:45:28 +0600

    Create menu.php

---
+++ блоки/menu.php
@@ -0,0 +1,11 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3
+px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <h5 class="my-0 mr-md-auto font-weight-5px">Dann.Co</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Главная</a>
+            <a class="p-2 text-dark" href="#">Контакты</a>
+            <a class="p-2 text-dark" href="#">Оставить заявку</a>
+            <a class="p-2 text-dark" href="#">Тарифы</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="#">Войти</a>
+    </div>
